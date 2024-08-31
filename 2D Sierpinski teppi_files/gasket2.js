53e15d02508bb4ed3f3aff1234f5d563a41632e4
Ásdís Valtýsdóttir <asdisval@gmail.com>
2024-08-31 16:16:29 +0000

    komi naestum med daemi 5

--- 2D Sierpinski teppi_files/gasket2.js
+++ 2D Sierpinski teppi_files/gasket2.js
@@ -3,9 +3,9 @@
 var canvas;
 var gl;
 
-var points = [];
+var punktar = [];
 
-var NumTimesToSubdivide = 5;
+var FjoldiSkiptinga = 4; 
 
 window.onload = function init()
 {
@@ -20,23 +20,15 @@ window.onload = function init()
 
     // First, initialize the corners of our gasket with three points.
 
-    var vertices = [
+    var hnutar = [
         vec2( -1, -1 ),
-        vec2( -1, 1),
-        vec2( 1, 1),
-        //þríhyrningur 1
-        vec2( 1 ,1),
-        vec2(  1, -1)
-        vec2(-1, -1)
-        // þríhyrningur 2
+        vec2(-1, 1),
+        vec2(1, -1),
+        vec2(1, 1),
     ];
 
-    divideTriangle( vertices[0], vertices[1], vertices[2],
-                    NumTimesToSubdivide);
-    
-    divideTriangle(vertices[3], vertices[4], vertices[5], NumTimesToSubdivide);
+    butumFerning(hnutar[0], hnutar[1], hnutar[2], hnutar[3], FjoldiSkiptinga);
 
-    
     //
     //  Configure WebGL
     //
@@ -52,7 +44,7 @@ window.onload = function init()
 
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(punktar), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
 
@@ -63,39 +55,52 @@ window.onload = function init()
     render();
 };
 
-function triangle( a, b, c )
-{
-    points.push( a, b, c );
+// Fallið @ferningur mun hlaða ferningnum inn í punktafylkið. Punktafylkið hefur tengingu við litina.
+function ferningur (a, b, c, d) {
+punktar.push(a, b, c, d);
+
 }
 
-function divideTriangle( a, b, c, count )
-{
+function butumFerning(a, b, c, d, skipting){
+    if (skipting === 0) {
+        ferningur(a, b, c, d);
+    } else {
+        //Finnum punkta fyrir 8 ferninga!
 
-    // check for end of recursion
+        //Ytri punktar
+        var ab = mix(a, b, 1/3);
+        var ab2 = mix(a, b, 2/3);
+        var bd = mix(b, d, 1/3);
+        var bd2 = mix(b, d, 2/3);
+        var cd = mix(c, d, 1/3);
+        var cd2 = mix(c, d, 2/3);
+        var ac = mix(a, c, 1/3);
+        var ac2 = mix(a, c, 2/3);
 
-    if ( count === 0 ) {
-        triangle( a, b, c );
-    }
-    else {
+        //Innri punktar
+        var ad = mix(a, d, 1/3);
+        var ad2 = mix(a, d, 2/3);
+        var bc = mix(b, c, 1/3);
+        var bc2 = mix(b, c, 2/3);
 
-        //bisect the sides
+        //fækkum skiptingu um einn
+        --skipting;
 
-        var ab = mix( a, b, 0.5 );
-        var ac = mix( a, c, 0.5 );
-        var bc = mix( b, c, 0.5 );
-
-        --count;
-
-        // three new triangles
-
-        divideTriangle( a, ab, ac, count );
-        divideTriangle( c, ac, bc, count );
-        divideTriangle( b, bc, ab, count );
+        //bútum ferninginn niður í átta ferninga!
+        butumFerning(a, ab, ac, ad, skipting);
+        butumFerning(ab, ab2, ad, bc, skipting);
+        butumFerning(ab2, b, bc, bd, skipting);
+        butumFerning(bc, bd, ad2, bd2, skipting);
+        butumFerning(ad2, bd2, cd2, d, skipting);
+        butumFerning(bc2, ad2, cd, cd2, skipting);
+        butumFerning(ac2, bc2, c, cd, skipting);
+        butumFerning(ac, ad, ac2, bc2, skipting);
+        
     }
 }
 
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, points.length );
+    gl.drawArrays( gl.TRIANGLE_STRIP, 0, punktar.length );
 }
