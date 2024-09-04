7332e89f5aae8207efc7bd81eb8a851faf979e1a
Ásdís Valtýsdóttir <asdisval@gmail.com>
2024-09-04 21:25:48 +0000

    box komid, bara ekki ad hreyfast

--- Einstaklingsverkefni1/spadi_files/spadi.js
+++ Einstaklingsverkefni1/spadi_files/spadi.js
@@ -7,9 +7,14 @@
 var canvas;
 var gl;
 
+var colorloc;
+
 
 var mouseX;             // Old value of x-coordinate  
 var movement = false;   // Do we move the paddle?
+
+//var maxX = 1.0;
+//var triangleRad = 0.1;
 
 window.onload = function init() {
 
@@ -19,7 +24,7 @@ window.onload = function init() {
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+    gl.clearColor( 0.9, 0.9, 0.8, 1.0 );
 
     //
     //  Load shaders and initialize attribute buffers
@@ -29,10 +34,17 @@ window.onload = function init() {
     
     var vertices = [
         vec2( -0.1, -0.9 ),
-        vec2( -0.1, -0.86 ),
-        vec2(  0.1, -0.86 ),
-        vec2(  0.1, -0.9 ) 
+        vec2( 0.0, -0.8 ),
+        vec2( 0.1, -0.9 ),
+
+        vec2( 0.8, 0.8 ),
+        vec2( 0.9, 0.8 ),
+        vec2( 0.8, 0.9 ),
+        vec2( 0.9, 0.9 ),
+        vec2( 0.9, 0.8 ),
+        vec2( 0.8, 0.9 ),
     ];
+    
     
     // Load the data into the GPU
     var bufferId = gl.createBuffer();
@@ -43,6 +55,9 @@ window.onload = function init() {
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
+
+    colorloc = gl.getUniformLocation( program, "fcolor" );
+
 
     // Event listeners for mouse
     canvas.addEventListener("mousedown", function(e){
@@ -58,10 +73,9 @@ window.onload = function init() {
         if(movement) {
             var xmove = 2*(e.offsetX - mouseX)/canvas.width;
             mouseX = e.offsetX;
-            for(i=0; i<4; i++) {
+            for(i=0; i<3; i++) {
                 vertices[i][0] += xmove;
             }
-
             gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
         }
     } );
@@ -73,7 +87,7 @@ window.onload = function init() {
 function render() {
     
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+    gl.drawArrays( gl.TRIANGLES, 0, 9 );
 
     window.requestAnimFrame(render);
 }
