40ca93aad8e22eaaf48084938b1914d430f5ce33
Ásdís Valtýsdóttir <asdisval@gmail.com>
2024-09-13 18:47:23 +0000

    finiseringar

--- Einstaklingsverkefni1/main.js
+++ Einstaklingsverkefni1/main.js
@@ -21,31 +21,31 @@ var bullets = [];
 var birds = [];
 var scorePoint = [];
 
-window.onload = function init() {
+window.onload = function init() {                                                           /* Comments for function init:*/
     const canvas = document.getElementById("gl-canvas");
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl)  alert("WebGL isn't available"); 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.55, 0.9, 0.8, 1.0 );
     
-    const program = initShaders(gl, "vertex-shader", "fragment-shader");        /* Load shaders and initialize attribute buffers */
+    const program = initShaders(gl, "vertex-shader", "fragment-shader");                    /* Load shaders and initialize attribute buffers */
     gl.useProgram(program);
 
-    vPosition = gl.getAttribLocation( program, "vPosition" );       /* Associate out shader variables with our data buffer */
+    vPosition = gl.getAttribLocation( program, "vPosition" );                               /* Associate out shader variables with our data buffer */
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    bufferForGun = gl.createBuffer();        /*Create a buffer for the gun */
+    bufferForGun = gl.createBuffer();                                                       /*Create a buffer for the gun */
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferForGun);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(gun), gl.DYNAMIC_DRAW);
     
-    var bulletSpace = new Float32Array(8);      /* Create a space for the bullets */
-    bufferForBullet = gl.createBuffer();        /* Create a buffer for the bullets */
+    var bulletSpace = new Float32Array(8);                                                  /* Create a space for the bullets */
+    bufferForBullet = gl.createBuffer();                                                    /* Create a buffer for the bullets */
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferForBullet);
     gl.bufferData(gl.ARRAY_BUFFER, bulletSpace, gl.DYNAMIC_DRAW);
     
-    var birdSpace = new Float32Array(birdCount*8);      /* Create some space for the birds */
-    bufferForBird = gl.createBuffer();      /* Create some buffer for the birds */
+    var birdSpace = new Float32Array(birdCount*8);                                          /* Create some space for the birds */
+    bufferForBird = gl.createBuffer();                                                      /* Create some buffer for the birds */
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferForBird);
     gl.bufferData(gl.ARRAY_BUFFER, birdSpace, gl.DYNAMIC_DRAW);
     
@@ -74,7 +74,7 @@ window.onload = function init() {
         }
     });
 
-    window.addEventListener("keydown", e => {         /* Event listener for spacebar */ 
+    window.addEventListener("keydown", e => {                                               /* Event listener for spacebar */ 
         if(e.code === "Space" && bullets.length < 3) {
             bullets.push({x: gun[1][0], y: -0.7, speed: 0.05});   
         }
@@ -83,12 +83,12 @@ window.onload = function init() {
     render();
 }
 
-function createRandomBirds(count) {
+function createRandomBirds(count) {                         /* Comments for function createRandomBirds:*/
     var createBirds = [];
 
     for(let i = 0; i < count; i++) {
-        var xPosition = Math.random() * 2 - 1;      /* Random x between -1 and 1 */
-        var yPosition = Math.random() * 0.8;        /* Random y between 0 and 0.8*/
+        var xPosition = Math.random() * 2 - 1;              /* Random x between -1 and 1 */
+        var yPosition = Math.random() * 0.8;                /* Random y between 0 and 0.8*/
         var baseSpeed = Math.random() * 0.02 + 0.005;       /* Random bird speed between 0.005 and 0.025*/
         var direction = Math.random() > 0.5 ? 1 : -1;       /* Randomly choose direction (1 or -1) */ 
         var speed = baseSpeed * direction;
@@ -99,20 +99,20 @@ function createRandomBirds(count) {
 }
 
 
-function drawVertices() {
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferForGun);       /* First we add the gun */
+function drawVertices() {                                                       /* Comments for function drawVertices:*/
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferForGun);                               /* First we add the gun */
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
     
-    for(var i = 0; i < birds.length; i++) {     /* Then draw the birds */
+    for(var i = 0; i < birds.length; i++) {                                     /* Then draw the birds */
         var bird = birds[i];
         bird.x += bird.speed;
         
-        let offset = Float32Array.BYTES_PER_ELEMENT*i*8;        /* Then specify (in bytes) where the data replacement will start */
+        let offset = Float32Array.BYTES_PER_ELEMENT*i*8;                        /* Then specify (in bytes) where the data replacement will start */
         
         if(bird.x > 1.1){ 
             bird.x = -1.1; 
-        }                       /* Let's make birds teleport */
+        }                                                                       /* Let's make birds teleport */
         if(bird.x < -1.1) { 
             bird.x = 1.1; 
         }       
@@ -127,12 +127,12 @@ function drawVertices() {
         gl.bufferSubData(gl.ARRAY_BUFFER, offset, flatten(birdVertices));
     }
     
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);        /* We need to bind the buffer to vPosition */
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);                /* We need to bind the buffer to vPosition */
     for(var i = 0; i < birds.length; i++) {
         gl.drawArrays(gl.TRIANGLE_FAN, i*4, 4);
     }
 
-    for(var i = 0; i < bullets.length; i++) {       /* finally draw the bullets */
+    for(var i = 0; i < bullets.length; i++) {                                   /* finally draw the bullets */
         var bullet = bullets[i];
         bullet.y += bullet.speed;
         var bulletVertices = [
@@ -171,7 +171,11 @@ function detectCollisions() {
                 bottom: bird.y - 0.01
             };
 
-            if (bulletBounds.right > birdBounds.left && bulletBounds.left < birdBounds.right && bulletBounds.top > birdBounds.bottom && bulletBounds.bottom < birdBounds.top) {
+            if (bulletBounds.right > birdBounds.left
+                 && bulletBounds.left < birdBounds.right 
+                 && bulletBounds.top > birdBounds.bottom 
+                 && bulletBounds.bottom < birdBounds.top) {
+                    
                 bullets.splice(i, 1);
                 birds.splice(j, 1);
                 addPoint();
